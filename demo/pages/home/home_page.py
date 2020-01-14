106a697a221c823ab6ab654f31a481f3e9441f56
Harvnlenny <Harvnlenny@gmail.com>
2020-01-13 20:47:54 -0500

    changed locator

--- demo/pages/home/home_page.py
+++ demo/pages/home/home_page.py
@@ -15,7 +15,7 @@ class HomePage(SeleniumDriver):
         self.send_keys(player_name, self._search_name, locator_type="name")
 
     def click_search_button(self):
-        self.element_click(self._search_button, locator_type="class")
+        self.element_click(self._search_button, locator_type="xpath")
 
     def player_search(self, player_name):
         self.enter_player_name(player_name)
